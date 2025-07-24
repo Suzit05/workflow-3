407bbcb5f39939b83b68d5b52d0b3a16416cd65d
Suzit05 <103447157+Suzit05@users.noreply.github.com>
2025-07-24 15:24:27 +0530

    v5

--- test-words-counter.py
+++ test-words-counter.py
@@ -3,6 +3,6 @@ from words-counter import file_to_test
 
 def test_file_to_test():
     test_filename="file.txt"
-    file_to_test(test_filename)
+    result= file_to_test(test_filename)
 
-   assert file_to_test("file.txt") == 8
+    assert result== 8
